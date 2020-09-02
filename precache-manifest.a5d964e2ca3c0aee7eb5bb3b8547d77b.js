b63297109655f13920cfc3e6347f668a62ce8793
Yolxi <jacacanada@gmail.com>
2020-09-02 02:07:51 -0400

    Updates

---
+++ precache-manifest.a5d964e2ca3c0aee7eb5bb3b8547d77b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a16f39de8d4d160898734f91f06ad5bc",
+    "url": "/Color-maker/index.html"
+  },
+  {
+    "revision": "7f6c1fb9abbb0d0bb2eb",
+    "url": "/Color-maker/static/css/2.ed590494.chunk.css"
+  },
+  {
+    "revision": "796b67d7d0628c38906c",
+    "url": "/Color-maker/static/css/main.1ec16eed.chunk.css"
+  },
+  {
+    "revision": "7f6c1fb9abbb0d0bb2eb",
+    "url": "/Color-maker/static/js/2.cf22911c.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/Color-maker/static/js/2.cf22911c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "796b67d7d0628c38906c",
+    "url": "/Color-maker/static/js/main.5bd1422a.chunk.js"
+  },
+  {
+    "revision": "17f38bf6261ffcbfbbf2",
+    "url": "/Color-maker/static/js/runtime-main.5d296de7.js"
+  },
+  {
+    "revision": "77428cd498bf82179ff99fdb246b2494",
+    "url": "/Color-maker/static/media/bg.77428cd4.svg"
+  }
+]);
